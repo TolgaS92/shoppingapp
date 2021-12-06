dca877c23d83b37cba5dfef743f0a1e7bc81050f
Tolga Secme <tolgasecme@icloud.com>
2021-12-05 17:48:14 -0700

    not found/browserrouter/switch/Router added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import CategoryList from "./CategoryList";
 import NavigationBar from "./NavigationBar";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
+import alertify from "alertifyjs";
+import { Route, Switch } from "react-router-dom";
+import NotFound from "./NotFound";
+import CartList from "./CartList";
 
 export default class App extends Component {
   state = {
@@ -40,15 +44,22 @@ export default class App extends Component {
       newCart.push({ product: product, quantity: 1 });
     }
     this.setState({ cart: newCart });
+    alertify.success(product.productName + " added to cart!", 2);
+  };
+
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter((c) => c.product.id !== product.id);
+    this.setState({ cart: newCart });
   };
   render() {
     let productInfo = { title: "ProductList" };
     let categoryInfo = { title: "CategoryList" };
     return (
-      <Container>
-        <Row>
-          <NavigationBar cart={this.state.cart} />
-        </Row>
+      <Container fluid>
+        <NavigationBar
+          removeFromCart={this.removeFromCart}
+          cart={this.state.cart}
+        />
         <Row>
           <Col xs="3">
             <CategoryList
@@ -58,13 +69,24 @@ export default class App extends Component {
             />
           </Col>
           <Col xs="9">
-            <ProductList
-              addToCart={this.addToCart}
-              products={this.state.products}
-              currentCategory={this.state.currentCategory}
-              changeCategory={this.changeCategory}
-              info={productInfo}
-            />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <ProductList
+                    {...props}
+                    addToCart={this.addToCart}
+                    products={this.state.products}
+                    currentCategory={this.state.currentCategory}
+                    changeCategory={this.changeCategory}
+                    info={productInfo}
+                  />
+                )}
+              />
+              <Route exact path="/cart" component={CartList}></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
           </Col>
         </Row>
       </Container>
--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -7,79 +7,46 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+} from "reactstrap";
+import CartSummary from "./CartSummary";
 
 export default class NavigationBar extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
   }
-  toggle(){
+  toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
     });
   }
   render() {
     return (
       <div>
-  <Navbar
-    color="warning"
-    expand="md"
-    light
-  >
-    <NavbarBrand href="/">
-      ShopFair
-    </NavbarBrand>
-    <NavbarToggler onClick={function noRefCheck(){}} />
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        <NavItem>
-          <NavLink href="/components/">
-            Components
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reactstrap/reactstrap">
-            GitHub
-          </NavLink>
-        </NavItem>
-        <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Options {this.props.cart.length}
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Option 1
-            </DropdownItem>
-            <DropdownItem>
-              Option 2
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>
-              Reset
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-    </Collapse>
-  </Navbar>
-</div>
+        <Navbar color="warning" expand="md" light>
+          <NavbarBrand href="/">ShopFair</NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() {}} />
+          <Collapse navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <CartSummary
+                removeFromCart={this.props.removeFromCart}
+                cart={this.props.cart}
+              />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
